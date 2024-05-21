53e81d46a492331257f25487a7982c7368009244
Georg Müller <georg.mueller@student.htl-rankweil.at>
2024-05-21 11:54:35 +0200

    Added SRC

---
+++ src/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace G04_DBI_Trainings_Adventure
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
