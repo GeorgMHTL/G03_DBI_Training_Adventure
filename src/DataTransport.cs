20c3240bd2290fe5da7ba20f3bb2dce67b7ef211
Georg Müller <georg.mueller@student.htl-rankweil.at>
2024-06-04 12:01:34 +0200

    Added Components and DataTransport.cs

---
+++ src/DataTransport.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace G04_DBI_Trainings_Adventure
+{
+   
+    public class DataTransport
+    {
+        private string srcString;
+        
+        public DataTransport(string srcString)
+        {
+            this.srcString = srcString;
+        }
+
+        public void LoadWorkouts(WrapPanel HomeWrap)
+        {
+            using (SqliteConnection connection = new SqliteConnection(srcString))
+            {
+                connection.Open();
+                SqliteCommand command = connection.CreateCommand();
+
+
+                command.CommandText = @"";
+
+                using (SqliteDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                       
+                      
+
+
+
+
+                    }
+                }
+
+            }
+
+
+
+        }
+
+        public void LoadExercises(string Datum, StackPanel ExcersiseStack)
+        {
+            
+        }
+
+    }
+}
