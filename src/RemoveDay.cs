0098e9cc31e9e270ae57955f778be8683baa5030
EdgeLordJ <joudi.khalil@student.htl-rankweil.at>
2024-06-13 17:57:14 +0200

    Base of Delete Day Function complete missing: how to get the date

---
+++ src/RemoveDay.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G04_DBI_Trainings_Adventure
+{
+    public class RemoveDay
+    {
+        private string date;
+
+        public string Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+        public RemoveDay()
+        {
+            Date = "";
+        }
+
+        public RemoveDay(string date)
+        {
+            Date = date;
+        }
+
+        public void RemoveDayFromTraining()
+        {
+            using (SqliteConnection connection = new SqliteConnection("Data Source=assets/TrainingsDoku.db"))
+            {
+                connection.Open();
+                SqliteCommand command = connection.CreateCommand();
+
+                command.CommandText = $"DELETE FROM Training WHERE fkTag = (SELECT ID FROM Trainingstage WHERE Datum = '{Date}');";
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void RemoveDayFromTrainingstage()
+        {
+            using (SqliteConnection connection = new SqliteConnection("Data Source=assets/TrainingsDoku.db"))
+            {
+                connection.Open();
+                SqliteCommand command = connection.CreateCommand();
+
+                command.CommandText = $"DELETE FROM Trainingstage WHERE Datum = '{Date}';";
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
